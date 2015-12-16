19aaa93f5fc466917805a6f222d7837e95e423a1
lhwsa2010 <754240664@qq.com>
2015-12-16 16:18:54 +0800

    add sidemenu

--- www/app/app.ts
+++ www/app/app.ts
@@ -1,22 +1,56 @@
-import {App, Platform} from 'ionic-framework/ionic';
-import {HomePage} from './home/home';
+import {App, IonicApp, Platform} from 'ionic-framework/ionic';
+
+import {GettingStartedPage} from './getting-started/getting-started';
+import {ListPage} from './list/list';
+import {GridPage} from './grid/grid';
+
 
 @App({
-  template: `
-    <ion-nav [root]="root"></ion-nav>
-    <ion-overlay></ion-overlay>
-  `,
+  templateUrl: 'app/app.html'
 })
-export class MyApp {
-  constructor(platform: Platform) {
+class MyApp {
+  constructor(app: IonicApp, platform: Platform) {
+    this.app = app;
     this.platform = platform;
+
     this.initializeApp();
-    this.root = HomePage;
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Getting Started', component: GettingStartedPage },
+      { title: 'List', component: ListPage },
+      { title: 'Grid Icons', component: GridPage },
+    ];
+
+    this.rootPage = GettingStartedPage;
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       console.log('Platform ready');
+
+      // The platform is now ready. Note: if this callback fails to fire, follow
+      // the Troubleshooting guide for a number of possible solutions:
+      //
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      //
+      // First, let's hide the keyboard accessory bar (only works natively) since
+      // that's a better default:
+      //
+      // Keyboard.setAccessoryBarVisible(false);
+      //
+      // For example, we might change the StatusBar color. This one below is
+      // good for dark backgrounds and light text:
+      // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
+
     });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    let nav = this.app.getComponent('nav');
+    nav.setRoot(page.component);
   }
 }
